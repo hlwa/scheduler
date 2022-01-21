485e0b4ac295e6f1765001585db8e4911742b3c6
Hailan Wang <hailan6257@gmail.com>
2022-01-20 22:23:34 -0500

    Built and test helpers

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) {
+    return []
+  }
+  const filteredDay = state.days.filter(e => e.name === day);
+  if (filteredDay.length === 0) {
+    return []
+  }
+  let selectedAppointments = [];
+  filteredDay[0].appointments.map(id => selectedAppointments.push(state.appointments[id]));
+  return selectedAppointments;
+}
